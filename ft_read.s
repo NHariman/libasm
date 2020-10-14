3e96475ac1b1753e58656ea808d0b6d77dbe8a78
Niks Hariman <nhariman@Niks-MacBook-Air.local>
2020-10-14 16:58:22 +0200

    added read

--- ft_read.s
+++ ft_read.s
@@ -5,18 +5,16 @@ section .text
 ; https://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/ sys call codes
 ; ssize_t		ft_read(int fd/rdi, const void *buf/rsi, size_t count/rdx);
 _ft_read:
-	push rbx ; push the count number onto the stack
-	mov rax, 0 ; move 0 into rax to call sys_read
+	mov rax, 0x2000003 ; move 0 into rax to call sys_read
 	syscall ; perform the system call
-	jc check_error;check for error
-	pop rbx ; pop the original count number back in
+	jc check_error ;check for error
 	ret
 
 check_error:
 	push rax ; push the value stored in rax
-	call ___error
-	mov rdi, rax
-	pop rax
-	mov [rdi], rax
-	mov rax, -1
-	ret
+	call ___error ; set errno, moves this value into rax
+	mov rdi, rax ; move this value into rdi
+	pop rax ; pop original value, the error number from the syscall into rax
+	mov [rdi], rax ; move this value into the address or rdi
+	mov rax, -1 ; move -1 into rax for error.
+	ret ; return rax
